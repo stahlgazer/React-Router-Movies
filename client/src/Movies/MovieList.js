4f37945fc3a154ee098ee332eb3951e04380d92b
gavin stahl <stahlgavin@gmail.com>
2019-12-17 17:37:21 -0800

    I believe I finished MVP

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import {Link} from "react-router-dom";
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
@@ -30,8 +31,10 @@ const MovieList = props => {
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
+    
     <div className="movie-card">
-      <h2>{title}</h2>
+      <Link to={`/movies/${movie.id}`}>
+      <h2>{title}</h2></Link>
       <div className="movie-director">
         Director: <em>{director}</em>
       </div>
@@ -46,6 +49,7 @@ function MovieDetails({ movie }) {
         </div>
       ))}
     </div>
+    
   );
 }
 
